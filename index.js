2086f4ad1ef29f4de48c352b023d4d4a84adf524
SatooRu65536 <ryuto4619@gmail.com>
2022-12-25 02:11:25 +0900

    :pencil2: scss => ncss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { evaluate } from './src/interpriter.js';
 
-const fileName = process.argv.length > 2 ? process.argv[2] : 'main.scss';
+const fileName = process.argv.length > 2 ? process.argv[2] : 'main.ncss';
 fs.readFile(fileName, "utf-8", (err, data) => {
     if (err) throw console.log(err.message);
     console.log(evaluate(data));
